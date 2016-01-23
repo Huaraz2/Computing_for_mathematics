10bb8b3f8aef5e4605157bddba2ae3dcc931d1c7
Seva, the Russian <constituam@gmail.com>
2016-01-23 12:14:37 +0000

    Python 2 to 3
    Adding () for print

--- Assessment/ClassTest/2014-2015/Solutions/solutionq3.py
+++ Assessment/ClassTest/2014-2015/Solutions/solutionq3.py
@@ -16,14 +16,14 @@ def isperfect(n):
 
 # 20 marks for getting that function correct - but I expect very few answers will be that compact and also few will be correct (but marks still available)
 
-print "There are %s perfect numbers less than 10000" % len([n for n in range(1, 10001) if isperfect(n)])
+print("There are %s perfect numbers less than 10000" % len([n for n in range(1, 10001) if isperfect(n)]))
 
 # 10 marks for the above: again I expect students won't use a list comprehension but might make use of a for loop (which is fine):
 
 count = 0
 for k in range(1, 10001):  # If they're off by one don't penalise too heavily
     if isperfect(k):
-        print "%s is perfect" % k  # This outputs the perfect number to screen which was not asked
+        print("%s is perfect" % k)  # This outputs the perfect number to screen which was not asked
         count += 1
 
-print "There are %s perfect numbers less than 10000" % count
+print("There are %s perfect numbers less than 10000" % count)
